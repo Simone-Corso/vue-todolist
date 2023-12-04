e0238d801230079294183a0cc2384873ac28c981
Simone-Corso <scorso26@gmail.com>
2023-12-04 15:04:17 +0100

    ho inserito il titolo e messo il font cambria

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //creare la todo list, uguale come abbiamo fatto stamattina, facendo però due proprietà.
 
-  const { createApp } = Vue;
+  const { createApp } = 'Vue';
 
   createApp({
     data() {
