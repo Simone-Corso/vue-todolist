6daa897c70b91ae412ed8a8bb1408c4bd04908d3
Simone-Corso <scorso26@gmail.com>
2023-12-04 15:26:10 +0100

    ho inserito una array di oggetti, con la variabile di MyNew TodoList El per aggiungere poi se ne ho bisogno.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 //creare la todo list, uguale come abbiamo fatto stamattina, facendo però due proprietà.
 
+//creare un array degli oggetti, e inserire un todo per ogni oggetto.
+
   const { createApp } = 'Vue';
 
   createApp({
     data() {
       return {
-        
+        MyNewTodoListEl: '',
+        MytodoList : [
+            'andare a correre', 
+            'vincere una maratona',
+            'seguire a tempo la musica',
+            'viaggiare per il mondo'
+        ],
+
       }
     }
   }).mount('#app')
