cb2c28b77c55e3c4d1f3107c1a8dc7214ddc1377
Simone-Corso <scorso26@gmail.com>
2023-12-04 14:58:35 +0100

    inserito tutto quello che serviva per realizzare la todo list

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//creare la todo list, uguale come abbiamo fatto stamattina, facendo però due proprietà.
+
+  const { createApp } = Vue;
+
+  createApp({
+    data() {
+      return {
+        
+      }
+    }
+  }).mount('#app')
