79f8b8db4ec86ce6ba2a677ac603d6c09e20871e
Simone-Corso <scorso26@gmail.com>
2023-12-05 08:47:23 +0100

    inserito nel javascript la lista di oggetti

--- js/script.js
+++ js/script.js
@@ -2,40 +2,65 @@
 
 //creare un array degli oggetti, e inserire un todo per ogni oggetto.
 
-  const { createApp } = 'Vue';
+const { createApp } = Vue;
 
-  createApp({
+createApp({
     data() {
-      return {
-            listaTodos : [
-            {
-                text: 'devo andare in palestra',
-                done: 'true',
-            },
-            {
-                text: 'devo contare fino a 100',
-                done: 'false',
-            },
-            {
-                text: 'pulire la casa in 40 minuti',
-                done: 'true',
-            },
-            {
-                text: 'giocare a Warzone6',
-                done: 'false',
-            },
-            {
-                text: 'cominciare la dieta',
-                done: 'true',
-            },
+        return {
+            newTodoElement: '',
+            todoList : [
+                {
+                    text: 'devo andare in palestra',
+                    done: 'true',
+                },
+                {
+                    text: 'devo contare fino a 100',
+                    done: 'false',
+                },
+                {
+                    text: 'pulire la casa in 40 minuti',
+                    done: 'true',
+                },
+                {
+                    text: 'giocare a Warzone6',
+                    done: 'false',
+                },
+                {
+                    text: 'cominciare la dieta',
+                    done: 'true',
+                },
+    
+            ]
+          }
+          
+        },
+    
+    methods: {
+        addNewTodoItem(newElement){
+            newElement = newElement.trim();
+            console.warn(newElement);
 
-        ]
-      }
-      
+            const todoNewEl = this.stringtodoNewEl(newElement);
+
+            if (todoNewEl.length > 3 && this.todo.includes(todoNewEl)
+            && !this.todoList.includes(todoNewEl) ){
+                this.newTodoElement = '';
+                this.todoList.push(todoNewEl);
+            }
+        },
+
+        stringtodoNewEl(rawString){
+            rawString = rawString.trim().toLowerCase();
+            return rawString.charAt(0).toUpperCase() + rawString.slice(1);
+        },
+
+        removeTodoElement(indexToBeRemoved){
+            console.log(indexToBeRemoved);
+            this.todoList.splice(indexToBeRemoved, 1);
+        }
     },
 
-    methods:{
-        
-    }
+    created() {
+    },
+}).mount('#app');
     
-  }).mount('#app')
